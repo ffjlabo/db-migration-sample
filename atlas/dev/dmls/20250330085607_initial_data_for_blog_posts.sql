2abcd64dff0f7035f95797820dcf0d46a3285eaf
Yoshiki Fujikane <ffjlabo@gmail.com>
2025-03-30 18:08:41 +0900

    Insert initial data for blog_posts

---
+++ atlas/dev/dmls/20250330085607_initial_data_for_blog_posts.sql
@@ -0,0 +1,2 @@
+INSERT INTO "blog_posts" (id, title, body, author_id) VALUES (1, 'My First Blog Post', 'This is my first blog post', 1);
+INSERT INTO "blog_posts" (id, title, body, author_id) VALUES (2, 'My First Blog Post', 'This is my first blog post', 2);
